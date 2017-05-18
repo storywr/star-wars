47df6564022bf745628142991b1cb4f2f1f50364
Bill <storywr@gmail.com>
2017-05-18 14:05:37 -0400

    cleaned up ruby code

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -7,16 +7,12 @@ class Api::CharactersController < ApplicationController
   end
 
   def characters
-    response = HTTParty.get('http://swapi.co/api/people/?format=json')
-    response_two = HTTParty.get('http://swapi.co/api/people/?format=json&page=2')
-    response_three = HTTParty.get('http://swapi.co/api/people/?format=json&page=3')
-    response_four = HTTParty.get('http://swapi.co/api/people/?format=json&page=4')
-    response_five = HTTParty.get('http://swapi.co/api/people/?format=json&page=5')
-    response_six = HTTParty.get('http://swapi.co/api/people/?format=json&page=6')
-    response_seven = HTTParty.get('http://swapi.co/api/people/?format=json&page=7')
-    response_eight = HTTParty.get('http://swapi.co/api/people/?format=json&page=8')
-    response_nine = HTTParty.get('http://swapi.co/api/people/?format=json&page=9')
-    character_array = [response, response_two, response_three, response_four, response_five, response_six, response_seven, response_eight, response_nine]
+    i = 1
+    character_array = []
+    while i < 10 do
+      character_array.push(HTTParty.get("http://swapi.co/api/people/?format=json&page=#{i}"))
+      i += 1
+    end
     render json: character_array
   end
 
